848347c06ff4471e36bcc0e662f8b66ab2b1a508
aabalalonso <aabalalonso@atanasoff16.danielcastelao.org>
2016-01-25 09:10:57 +0100

    commit correjido de unos errores

--- src/listarcontactos/Libreta.java
+++ src/listarcontactos/Libreta.java
@@ -7,34 +7,32 @@ import javax.swing.JOptionPane;
  * @author Miguel
  */
 public class Libreta {
-    Contacto propietario = new Contacto();
-    Contacto [] arrayContactos = new Contacto [10];
-    //for(int i=0;i<arrayContactos.length;i++){
-        //arrayContactos[i].setNombre("");
-    int libre= cualLibre();
-    public void engadir (){
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Grupo[]grupoFamilia= new Grupo[10];
+        Grupo miLibreta = new Grupo();
+        miLibreta.engadir();
+        miLibreta.listar();
+        boolean condicion = true ;    
+        do{}while(condicion);
+            switch(seleccion){
+                case 1 = 
+            
+            
+            
+            
+            
+            
+            
+            
+            }
+            
+            
+            
+    }
+    }
     
-     if(libre<0){
-         JOptionPane.showMessageDialog(null,"No hay hueco.");
-     } else {
-         PedirDatos();
-     }   
-    }
-    public int cualLibre(){
-    for(int i=0;i<arrayContactos.length;i++){      
-    if(arrayContactos[i].equals("")){
-        return i;
-    }
-    }
-        return -1;
-}
- public Contacto PedirDatos(){
-     String aux_nombre = JOptionPane.showInputDialog("Introduce el nombre:");
-     String aux_apellido = JOptionPane.showInputDialog("Introduce el apellido:");
-     String aux_telefono = JOptionPane.showInputDialog("Introduce el telefono:");
-     String aux_email = JOptionPane.showInputDialog("Introduce el email:");
-     Contacto contacto = new Contacto();
-        return contacto;
-    
- }
-}
+
